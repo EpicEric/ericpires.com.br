d298aad1d92a5a5b8a88339abb0ac59fb13b4c49
Eric Pires <ericpires9@gmail.com>
2017-05-17 13:56:54 -0400

    UTF-8 strings for errors

--- homepage/error.py
+++ homepage/error.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, render_to_response
 
 def error404(request):
     context = {
         'error': 'Erro 404',
-        'errorname': 'Recurso nao encontrado',
-        'errordesc': 'A pagina que voce tentou acessar nao existe ou foi movida.',
+        'errorname': 'Recurso não encontrado',
+        'errordesc': 'A página que voce tentou acessar não existe ou foi movida.',
     }
     return render_to_response('homepage/error.html', context)
 
@@ -12,6 +15,6 @@ def error500(request):
     context = {
         'error': 'Erro 500',
         'errorname': 'Erro interno de servidor',
-        'errordesc': 'Nao foi possivel processar a solicitacao. Por favor, entre em contato com o administrador.',
+        'errordesc': 'Nao foi possível processar a solicitação. Por favor, entre em contato com o administrador.',
     }
     return render_to_response('homepage/error.html', context)
