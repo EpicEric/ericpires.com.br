b6a4ca84b89b1efe1e989f649faa53a8e615a9d9
Eric Pires <ericpires9@gmail.com>
2017-05-31 14:12:41 +0000

    Improve help display; use templates for command-specific help

--- codecompilerbot/views.py
+++ codecompilerbot/views.py
@@ -1,5 +1,6 @@
 import re
 import requests
+from django.template.loader import render_to_string
 from django.core.exceptions import ObjectDoesNotExist
 from telegrambot.models import Chat
 from telegrambot.bot_views.generic import TemplateCommandView, ListDetailCommandView, \
@@ -7,12 +8,21 @@ from telegrambot.bot_views.generic import TemplateCommandView, ListDetailCommand
 from .models import Language, Code
 
 
+# /start: Show basic help and commands
 class StartCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_start.txt"
 
-class HelpCommandView(TemplateCommandView):
-    template_text = "codecompilerbot/command_help.txt"
+    def get_context(self, bot, update, **kwargs):
+        context = {}
+        context['command'] = ''
+        chat_id = update.message.chat.id
+        try:
+            context['current_language'] = Code.objects.get(chat__id=chat_id).language
+        except ObjectDoesNotExist:
+            context['current_language'] = None
+        return context
 
+# /?????: Display unknown command error message
 class UnknownCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_unknown.txt"
     
@@ -22,10 +32,37 @@ class UnknownCommandView(TemplateCommandView):
             unknown_command = re.search('^(\/\w+).*', update.message.text).group(1)
         except AttributeError:
             unknown_command = ''
-        context['command'] = ' {}'.format(unknown_command) if unknown_command else ''
+        context['command'] = unknown_command.lower()
         return context
 
+# /help: Show list of commands or detailed help on a specific one
+class HelpCommandView(TemplateCommandView):
+    template_text = "codecompilerbot/command_help.txt"
+    
+    def get_context(self, bot, update, **kwargs):
+        context = {}
+        try:
+            command = re.search('^\s*(\w+).*', update.message.text[6:]).group(1)
+            context['command'] = '/{}'.format(command.lower())
+            help_dict = {
+                'help': render_to_string('codecompilerbot/help/help.txt'), 
+                'start': render_to_string('codecompilerbot/help/start.txt'),
+                'reset': render_to_string('codecompilerbot/help/reset.txt'),
+                'language': render_to_string('codecompilerbot/help/language.txt'),
+                'code': render_to_string('codecompilerbot/help/code.txt'),
+                'input': render_to_string('codecompilerbot/help/input.txt'),
+                'run': render_to_string('codecompilerbot/help/run.txt'),
+            }
+            known_command = command in help_dict
+            if known_command:
+                context['command_desc'] = help_dict[command]
+            else:
+                context['command_desc'] = ''
+        except AttributeError:
+            context['command'] = ''
+        return context
 
+# /language: Display a list of languages for selection
 class LanguageListCommandView(ListCommandView):
     template_text = "codecompilerbot/command_language_list.txt"
     template_keyboard = "codecompilerbot/command_language_list_keyboard.txt"
@@ -41,7 +78,8 @@ class LanguageListCommandView(ListCommandView):
         except ObjectDoesNotExist:
             context['current_language'] = None
         return context
-    
+
+# /language ?????: Select the provided language
 class LanguageDetailCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_language_selected.txt"
     language = None
@@ -80,7 +118,7 @@ class LanguageCommandView(ListDetailCommandView):
     list_view_class = LanguageListCommandView
     detail_view_class = LanguageDetailCommandView
 
-
+# /code: View current code or add new one
 class CodeCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_code.txt"
 
@@ -102,6 +140,7 @@ class CodeCommandView(TemplateCommandView):
             context['code'] = None
         return context
 
+# /input: View current input or add new one
 class InputCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_input.txt"
 
@@ -120,7 +159,7 @@ class InputCommandView(TemplateCommandView):
             context['code'] = None
         return context
 
-
+# /run: Code execution subroutine
 class RunCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_run.txt"
 
@@ -153,6 +192,7 @@ class RunCommandView(TemplateCommandView):
             context['code'] = None
         return context
 
+# /reset: Allow removing user specific information
 class ResetCommandView(TemplateCommandView):
     template_text = "codecompilerbot/command_reset.txt"
 
@@ -165,3 +205,4 @@ class ResetCommandView(TemplateCommandView):
             code = Code.objects.filter(chat__id=chat_id)
             code.delete()
         return context
+
