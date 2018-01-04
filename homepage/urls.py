37b78f0ca18d6aa7b54cb1b6b4cddba2a00ae37f
Eric Rodrigues Pires <ericpires9@gmail.com>
2018-01-04 18:08:08 +0000

    Add Keybase proof

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,3 +1,4 @@
+from django.templatetags.static import static
 from django.urls import path
 from django.urls.base import reverse_lazy
 from django.views.generic import RedirectView
@@ -9,9 +10,13 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('contato/', RedirectView.as_view(url=reverse_lazy('contact'))),
 
+    # Keybase
+    path('keybase.txt', RedirectView.as_view(url=static('homepage/keybase.txt'))),
+
     # Shorteners
-    path('steam/', RedirectView.as_view(url='http://steamcommunity.com/id/epiceric'), name='steam'),
     path('facebook/', RedirectView.as_view(url='https://www.facebook.com/ericpires9'), name='facebook'),
-    path('linkedin/', RedirectView.as_view(url='https://www.linkedin.com/in/eric-rodrigues-pires-19231190/'), name='linkedin'),
     path('github/', RedirectView.as_view(url='https://github.com/EpicEric'), name='github'),
+    path('keybase/', RedirectView.as_view(url='https://keybase.io/EpicEric'), name='keybase'),
+    path('linkedin/', RedirectView.as_view(url='https://www.linkedin.com/in/eric-rodrigues-pires-19231190/'), name='linkedin'),
+    path('steam/', RedirectView.as_view(url='http://steamcommunity.com/id/epiceric'), name='steam'),
 ]
